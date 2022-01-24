ac8bc26f6d4c256374b74af1e6e2fba08965eb0c
Readpato <87768087+Readpato@users.noreply.github.com>
2022-01-24 12:07:37 +0100

    feat: Create eventListener that shows next pokemons
    The event listener that activates the functions that show the succesive Pokemon on the Pokedex has been created. The Event listener will run a function that deletes all old Pokemon Card and load up new pokemon cards

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const POKEMON_LIST_URL = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
-const POKEMON_LIST_CONTAINER = document.querySelector(
-  ".pokemon-list-container"
-);
+const $pokemonListContainer = document.querySelector(".pokemon-list-container");
+const $lowerNextButton = document.querySelector(".lower-next-button");
+const $lowerPreviousButton = document.querySelector(".lower-previous-button");
+const $upperNextButton = document.querySelector(".upper-next-button");
+const $upperPreviousButton = document.querySelector(".upper-previous-button");
+let nextPokemonList;
+let previousPokemonList = null;
 
-function loadPokemonList(POKEMON_LIST_URL) {
-  return fetch(POKEMON_LIST_URL)
+function loadPokemonList(pokeApiURL) {
+  return fetch(pokeApiURL)
     .then((api_response) => {
       if (!api_response.ok)
         return "Something went wrong, please try again later";
       return api_response.json();
     })
     .then((api_responseJSON) => {
+      nextPokemonList = api_responseJSON.next;
       return api_responseJSON.results.forEach((object) => {
         loadSinglePokemon(object.url);
       });
@@ -66,7 +71,7 @@ function createPokemonCard(pokemon) {
   $pokemonCard.appendChild($pokemonImage);
   $pokemonCard.appendChild($pokemonCardBody);
 
-  return POKEMON_LIST_CONTAINER.appendChild($pokemonCard);
+  return $pokemonListContainer.appendChild($pokemonCard);
 }
 
 function capitalizeFirstLetter(string) {
@@ -90,3 +95,14 @@ function assessPokemonTypeQuantity(typeQuantity) {
 }
 
 loadPokemonList(POKEMON_LIST_URL);
+$lowerNextButton.addEventListener("click", () => {
+  deletePreviousPokemonCards();
+  loadPokemonList(nextPokemonList);
+});
+
+function deletePreviousPokemonCards() {
+  const $pokemonCards = document.querySelectorAll(".pokemon-card");
+  $pokemonCards.forEach((card) => {
+    card.remove();
+  });
+}
